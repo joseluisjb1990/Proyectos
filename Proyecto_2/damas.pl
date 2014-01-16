ea53522944cb9674d64b0f895c6b4938aecef91d
Jose Jimenez <jljb1990@gmail.com>
2014-01-15 21:15:58 -0430

    Come doble con un solo jugador

--- Proyecto_2/damas.pl
+++ Proyecto_2/damas.pl
@@ -14,34 +14,34 @@ jugador2(rey, '>>').
 
 turno(jugador1).
 
-ficha(1, 2, '<').
-ficha(1, 4, '<').
-ficha(1, 6, '<').
-ficha(1, 8, '<').
-ficha(2, 1, '<').
-ficha(2, 3, '<').
+%ficha(1, 2, '<').
+%ficha(1, 4, '<').
+%ficha(1, 6, '<').
+%ficha(1, 8, '<').
+%ficha(2, 1, '<').
+%ficha(2, 3, '<').
 ficha(2, 5, '<').
-ficha(2, 7, '<').
-ficha(3, 2, '<').
-ficha(3, 4, '<').
-ficha(3, 6, '<').
-ficha(3, 8, '<').
+%ficha(2, 7, '<').
+%ficha(6, 3, '<').
+%ficha(3, 4, '<').
+%ficha(3, 6, '<').
+%ficha(3, 8, '<').
 
-ficha(6, 1, '>').
-ficha(6, 3, '>').
-ficha(6, 5, '>').
-ficha(6, 7, '>').
-ficha(7, 2, '>').
-ficha(7, 4, '>').
-ficha(7, 6, '>').
-ficha(7, 8, '>').
-ficha(8, 1, '>').
-ficha(8, 3, '>').
-ficha(8, 5, '>').
-ficha(8, 7, '>').
+ficha(3, 6, '>').
+ficha(5, 6, '>').
+%ficha(6, 5, '>').
+%ficha(6, 7, '>').
+%ficha(7, 2, '>').
+%ficha(7, 4, '>').
+%ficha(7, 6, '>').
+%ficha(7, 8, '>').
+%ficha(8, 1, '>').
+%ficha(8, 3, '>').
+%ficha(8, 5, '>').
+%ficha(8, 7, '>').
 
-% jugar :- hacerPregunta, imprimirTablero, hacerMovimiento(jugador1(X)).
-jugar :- hacerPregunta, jugada.
+jugar :- hacerPregunta, imprimirTablero.
+% jugar :- hacerPregunta, jugada.
 
 hacerMovimiento(jugador1) :- jugador1(Y), elegirTipo(Y).
 hacerMovimiento(jugador2) :- jugador2(Y), elegirTipo(Y).
@@ -49,28 +49,51 @@ hacerMovimiento(jugador2) :- jugador2(Y), elegirTipo(Y).
 elegirTipo(maquina) :- moverMaquina.
 elegirTipo(persona) :- moverPersona.
 
-moverMaquina :- write('juega la maquina '), turno(X), write(X),  write('\n').
-moverPersona :- write('juega la persona '), turno(X), write(X),  write('\n').
+moverMaquina :- write('juega la maquina '), turno(X), write(X),  write('\n'), imprimirTablero.
+moverPersona :- write('juega la persona '), turno(X), write(X),  write('\n'), imprimirTablero.
 
 actualizarTurno(jugador1) :- retract(turno(jugador1)), assert(turno(jugador2)).
 actualizarTurno(jugador2) :- retract(turno(jugador2)), assert(turno(jugador1)).
 
 jugada :- turno(X), hacerMovimiento(X).
 
-jugada(X1,Y1,X2,Y2) :- fichaValida(X1,Y1), posicionValida(X2,Y2),turno(Z), elegirFicha(X1,Y1,X2,Y2,Z).
+jugada(X1,Y1,X2,Y2) :- hacerJugada(X1,Y1,X2,Y2).
+jugada(_,_,_,_)     :- write('Movimiento Invalido'), jugada.
 
-elegirFicha(X1,Y1,X2,Y2,jugador1) :- ficha(X1,Y1,Z), jugador1(W, Z), moverFichaJugador1(X1,Y1,X2,Y2,W).
+hacerJugada(X1,Y1,X2,Y2) :- posicionValida(X1,Y1), posicionValida(X2,Y2), turno(Z), fichaValida(X1,Y1,Z), elegirFicha(X1,Y1,X2,Y2,Z), imprimirTablero.
 
-moverFichaJugador1(X1,Y1,X2,Y2,peon)    :- 
-moverFichaJugador1(X1,Y1,X2,Y2,rey)     :-  
+comerDeNuevo(X1,Y1,X2,Y2) :- posicionValida(X1,Y1), posicionValida(X2,Y2), turno(Z), fichaValida(X1,Y1,Z), elegirFicha(X1,Y1,X2,Y2,Z).
 
-posicionValida(X,Y) :- X <= 8, X >= 1, Y <= 8, Y >= 1.
+comerDeNuevo(_,_,_,_).
 
-fichaValida(X,Y) :- posicionValida(X,Y), turno(Z), verificarFicha(X,Y,Z).
-fichaValida(_,_) :- write('No puedes mover la ficha seleccionada'), jugada.
+elegirFicha(X1,Y1,X2,Y2,Z) :- ficha(X1,Y1,Z), jugador1(W, Z), moverFichaJugador1(X1,Y1,X2,Y2,W).
 
-verificarFicha(X,Y,jugador1) :- ficha(X,Y,Z), jugador1(_,Z).
-verificarFicha(X,Y,jugador2) :- ficha(X,Y,Z), jugador2(_,Z).
+moverFichaJugador1(X1,Y1,X2,Y2,peon)    :-  (Y2 =:= Y1 - 1 ; Y2 =:= Y1 + 1), X2 =:= X1 + 1, not(ficha(X2,Y2,Z)), jugador1(peon, Z), retract(ficha(X1,Y1,Z)), turno(W), establecerFicha(W,X2,Y2).
+
+moverFichaJugador1(X1,Y1,X2,Y2,peon)    :-  Y2 =:= Y1 + 1 , X2 =:= X1 + 1, ficha(X2,Y2,Z), not(jugador1(_,Z)), jugador2(_,Z), Y3 is Y2 + 1, X3 is X2 + 1,
+                                            comerFicha(X1,Y1,X2,Y2,X3,Y3), repetirJugadaJugador1(X3,Y3).
+
+moverFichaJugador1(X1,Y1,X2,Y2,peon)    :-  Y2 =:= Y1 - 1 , X2 =:= X1 + 1, ficha(X2,Y2,Z), not(jugador1(_,Z)), jugador2(_,Z), Y3 is Y2 - 1, X3 is X2 + 1,
+                                            comerFicha(X1,Y1,X2,Y2,X3,Y3), repetirJugadaJugador1(X3,Y3).
+
+repetirJugadaJugador1(X,Y)  :- Y2 is Y - 1, X2 is X + 1, comerDeNuevo(X,Y,X2,Y2).
+
+repetirJugadaJugador1(X,Y)  :- Y2 is Y + 1, X2 is X + 1, comerDeNuevo(X,Y,X2,Y2).
+
+comerFicha(X1,Y1,X2,Y2,X3,Y3) :- posicionValida(X3,Y3), casillaVacia(X3,Y3), ficha(X1,Y1,W), ficha(X2,Y2,Z),
+                                 retract(ficha(X1,Y1,W)), retract(ficha(X2,Y2,Z)), turno(J), establecerFicha(J,X3,Y3).
+
+casillaVacia(X,Y) :- not(ficha(X,Y,_)).
+
+establecerFicha(jugador1,X,Y)            :-  X =:= 8, jugador1(rey, Z) , assert(ficha(X,Y,Z)).
+establecerFicha(jugador1,X,Y)            :-  jugador1(peon, Z) , assert(ficha(X,Y,Z)).
+
+% moverFichaJugador1(X1,Y1,X2,Y2,rey)     :-  
+
+posicionValida(X,Y) :- (X =< 8), (X >= 1), (Y =< 8), (Y >= 1).
+
+fichaValida(X,Y,jugador1) :- ficha(X,Y,Z), jugador1(_,Z).
+fichaValida(X,Y,jugador2) :- ficha(X,Y,Z), jugador2(_,Z).
 
 hacerPregunta :- write('\nDesea jugar contra la maquina? (S,N)\n'), read(X), actualizarJugadores(X).
 
@@ -90,4 +113,3 @@ imprimirFila(X) :- imprimirLista(X,1).
 imprimirLista(_,9).
 imprimirLista(F, C) :- ficha(F,C,Z), write(Z), write(' |  '), C1 is C + 1, imprimirLista(F,C1).
 imprimirLista(F, C) :- write('  |  ') , C1 is C + 1, imprimirLista(F,C1).
-
